f3d3ce33ddc8407fb61d4cd8612fe28623d0f062
Ajith <ajith@Venkats-MacBook-Pro.local>
2023-09-04 17:48:58 +0530

    scroll section sticky done

---
+++ src/pages/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <header className="hero__header">
+      <h1>Ajith</h1>
+      <h2>Developer</h2>
+    </header>
+  );
+};
+
+export default Hero;
---
+++ src/pages/components/ScrollSection.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+const ScrollSection = () => {
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const stick = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-300vw",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+    return () => {
+        stick.kill();
+    };
+  }, []);
+
+  return (
+    <section className="scroll-section-outer">
+      <div ref={triggerRef}>
+        <div ref={sectionRef} className="scroll-section-inner">
+          <div className="scroll-section">
+            <h3>Section 1</h3>
+          </div>
+          <div className="scroll-section">
+            <h3>Section 2</h3>
+          </div>
+          <div className="scroll-section">
+            <h3>Section 3</h3>
+          </div>
+          <div className="scroll-section">
+            <h3>Section 4</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ScrollSection;
